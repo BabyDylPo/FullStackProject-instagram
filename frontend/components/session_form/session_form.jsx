371896a132dbd079ec0bcfb854488d1a3de5ce81
dylpo <matthewsdb34@gmail.com>
2019-07-04 18:24:14 -0700

    add beginning of css for user auth

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -38,88 +38,93 @@ class SessionForm extends React.Component {
 
     render() {
         return (
-            <div className="page-container">
-                <div className="picture-container">
-                        <h2>picture goes here</h2>
-                    <div className="picture-container-picture">
-                        <h3>changing picture goes here</h3>
+            <article className="page-container">
+                <div className="page-container-box">
+                    <div className="picture-container">
+                            <h2>picture goes here</h2>
+                        <div className="picture-container-picture">
+                            <h3>changing picture goes here</h3>
+                        </div>
                     </div>
-                </div>
-            
-                <div className="form-container">
-                    <h1>Finstagram</h1>
-                    <div className="form-container-box">
-                        <form onSubmit={this.handleSubmit} className="form">
-                            <h2>Sign up to see fake photos and fake videos from your fake friends.</h2>
-                            <br/>
-                            {this.renderErrors()}
-                            <div className="submit-button-container">
-                                <input type="submit" value="Demo Login" className="submit-button"/>
-                            </div>
-                            <br/>
-                            <div className="form-separator">
-                                OR
-                            </div>
-                            <br/>
-                            <div className="form-label-container">
-                                <div className="form-label-container-box">
-                                    <label className="form-label">Username
-                                        <input 
-                                        type="text" 
-                                        value={this.state.username} 
-                                        onChange={this.update('username')}
-                                        className="form-input"
-                                        />
-                                    </label>
-                                </div>
+                
+                    <div className="form-container">
+                        <h1 className="form-header"><span>Finstagram</span></h1>
+                        <div className="form-container-box">
+                            <form onSubmit={this.handleSubmit} className="form">
+                                <h2>Sign up to see fake photos and fake videos from your fake friends.</h2>
                                 <br/>
-                                <div className="form-label-container-box">
-                                    <label className="form-label">Email
-                                        <input 
-                                        type="text" 
-                                        value={this.state.email} 
-                                        onChange={this.update('email')}
-                                        className="form-input"
-                                        />
-                                    </label>
-                                </div>
-                                <br/>
-                                <div className="form-label-container-box">
-                                    <label className="form-label">Password
-                                        <input 
-                                        type="password" 
-                                        value={this.state.password} 
-                                        onChange={this.update('password')}
-                                        className="form-input"
-                                        />
-                                    </label>
-                                </div>
-                                <br/>
+                                {this.renderErrors()}
                                 <div className="submit-button-container">
-                                    <input className="submit-button" type="submit" value={this.props.formType} />
+                                    <input type="submit" value="Demo Login" className="submit-button"/>
                                 </div>
-                                <div className="terms-container">
-                                    <p>By signing up, you agree to our 
-                                        ... list of things you are not going to read.
-                                    </p>
+                                <br/>
+                                <div className="form-separator">
+                                    OR
                                 </div>
+                                <br/>
+                                <div className="form-label-container">
+                                    <div className="form-label-container-box">
+                                        <label className="form-label">
+                                            <input 
+                                            type="text" 
+                                            value={this.state.username} 
+                                            onChange={this.update('username')}
+                                            className="form-input"
+                                            placeholder="Username"
+                                            />
+                                        </label>
+                                    </div>
+                                    <br/>
+                                    <div className="form-label-container-box">
+                                        <label className="form-label">
+                                            <input 
+                                            type="text" 
+                                            value={this.state.email} 
+                                            onChange={this.update('email')}
+                                            className="form-input"
+                                            placeholder="Email"
+                                            />
+                                        </label>
+                                    </div>
+                                    <br/>
+                                    <div className="form-label-container-box">
+                                        <label className="form-label">
+                                            <input 
+                                            type="password" 
+                                            value={this.state.password} 
+                                            onChange={this.update('password')}
+                                            className="form-input"
+                                            placeholder="Password"
+                                            />
+                                        </label>
+                                    </div>
+                                    <br/>
+                                    <div className="submit-button-container">
+                                        <input className="submit-button" type="submit" value={this.props.formType} />
+                                    </div>
+                                    <div className="terms-container">
+                                        <p>By signing up, you agree to our 
+                                            ... list of things you are not going to read.
+                                        </p>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="session-link-container">
+                            <p>Have an account? </p>
+                            <Link to="/login" className="session-link">log in</Link>
+                        </div>
+                        <div className="download-link-container">
+                            <p>Get the app.</p>
+                            <div className="download-link-container-box">
+                                <a className="download-link" href="https://itunes.apple.com/app/instagram/id389801252?pt=428156&amp;ct=igweb.loginPage.badge&amp;mt=8&amp;vt=lo">Apple Store</a>
+                                <br/>
+                                <a className="download-link" href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DXQ59hQAEAAFKbAmuaEfztY0eCuef%26utm_content%3Dlo%26utm_medium%3Dbadge">Google Store</a>
                             </div>
-                        </form>
-                    </div>
-                    <div className="session-link-container">
-                        <p>Have an account? </p>
-                        <Link to="/login" className="session-link">log in</Link>
-                    </div>
-                    <div className="download-link-container">
-                        <p>Get the app.</p>
-                        <div className="download-link-container-box">
-                            <a className="download-link" href="https://itunes.apple.com/app/instagram/id389801252?pt=428156&amp;ct=igweb.loginPage.badge&amp;mt=8&amp;vt=lo">Apple Store</a>
-                            <br/>
-                            <a className="download-link" href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DXQ59hQAEAAFKbAmuaEfztY0eCuef%26utm_content%3Dlo%26utm_medium%3Dbadge">Google Store</a>
                         </div>
                     </div>
                 </div>
-            </div>
+            </article>
         );
     }
 
